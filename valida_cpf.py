87e3b8c40377e1e6e18e9978feab40cd95907cf5
unknown <wesley.queiroz@fatec.sp.gov,br>
2024-05-13 23:05:47 -0300

    aula13

---
+++ valida_cpf.py
@@ -0,0 +1,34 @@
+def valida_cpf(cpf):
+    cpf = ''.join(filter(str.isdigit, cpf))
+
+    if len(cpf) != 11:
+        return False
+
+    if cpf == cpf[0] * 11:
+        return False
+
+    soma = 0
+    for i in range(9):
+        soma += int(cpf[i]) * (10 - i)
+    resto = soma % 11
+    if resto < 2:
+        digito1 = 0
+    else:
+        digito1 = 11 - resto
+
+    if digito1 != int(cpf[9]):
+        return False
+
+    soma = 0
+    for i in range(10):
+        soma += int(cpf[i]) * (11 - i)
+    resto = soma % 11
+    if resto < 2:
+        digito2 = 0
+    else:
+        digito2 = 11 - resto
+
+    if digito2 != int(cpf[10]):
+        return False
+
+    return True
